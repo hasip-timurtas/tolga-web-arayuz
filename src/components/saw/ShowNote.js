2e48138ed2553134807c00fa2f19557ff3cc46ef
hasip_timurtas <hasip.timurtas@gmail.com>
2018-12-08 11:31:11 +0300

    updates

--- src/components/saw/ShowNote.js
+++ src/components/saw/ShowNote.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { auth, db, GetNotes } from '../firebase'
-
+import "./style.css"
 export default class ShowNote extends React.Component {
     constructor(props) {
         super(props);
@@ -9,70 +9,38 @@ export default class ShowNote extends React.Component {
             content: this.props.selectedNotem.content
         };
     }
-
-    deleteNote() {
-        db.ref('Notes').child(auth.currentUser.uid).child(this.props.selectedNotem.fbId).set(null)
-        //Meteor.call("deleteNote", this.props.selectedNotem._id);
-        this.props.clearNote()
-    }
-
-    updateNote() {
-        const updatedData ={
-            title: this.props.selectedNote1.title,
-            content: this.props.selectedNote1.content,
-            _id: this.props.selectedNotem._id,
-            date: Date()
-        }
-        db.ref('Notes').child(auth.currentUser.uid).child(this.props.selectedNotem.fbId).set(updatedData)
-        //Meteor.call("updateNote", this.props.selectedNote1);
-    }
-
-    addNote(){
-        const newData ={
-            title: this.props.selectedNote1.title,
-            content: this.props.selectedNote1.content,
-            _id: '_' + Math.random().toString(36).substr(2, 9),
-            date: Date()
-        }
-        
-        db.ref('Notes').child(auth.currentUser.uid).push(newData)
-    }
-
+    
     render() {
+        const {selectedNotem} = this.props
+        const sizes = selectedNotem.validBedens.map(e=> <span className="size" data-toggle="tooltip" title={e}>{e}</span>)
+        const images = selectedNotem.images.map((e, i)=> <div className="tab-pane" id={"pic-"+i}><img src={e} /></div>)
+        const thumbs = selectedNotem.images.map((e, i)=> <li><a data-target={"pic-"+i} data-toggle="tab"><img src={e} /></a></li>)
         return (
             <div className="show-note">
-                <div>
-                    <input className="form-control"
-                        type="text"
-                        value={this.props.selectedNote1.title}
-                        ref="editInput"
-                        className="form-control"
-                        onChange={this.props.titleChange.bind(this)}
-                    />
-                 </div>
-                 <div style={{marginTop:'10px'}}>
-                    <textarea className="form-control note-content"
-                              placeholder="content" rows="15"
-                              value={this.props.selectedNote1.content}
-                              readonly={true}
-                              onChange={this.props.contentChange.bind(this)}>
-                    </textarea>
-                </div>
-
-                <div className="alert alert-info" role="alert">
-                    Entry Date : <strong>{/* note.entry_Date */}</strong>
-
-                    <button type="button"
-                            className="btn btn-default delete-note"
-                            onClick={this.deleteNote.bind(this)}
-                    >Delete
-                    </button>
-                    <button type="button"
-                            className="btn btn-default update-note"
-                            onClick={this.updateNote.bind(this)}
-                    >Update
-                    </button>
-                    <button type="button" className="btn btn-default share-note" onClick={this.addNote.bind(this)} >Add</button>
+                <div className="card">
+                    <div className="container-fliud">
+                        <div className="wrapper row">
+                            <div className="preview col-md-6">
+                                
+                                <div className="preview-pic tab-content">
+                                    <div className="tab-pane active" id="pic-1"><img src={selectedNotem.thumbImg} /></div>
+                                    {images}
+                                </div>
+                                <ul className="preview-thumbnail nav nav-tabs">
+                                    {thumbs}
+                                </ul>
+                                
+                            </div>
+                            <div className="details col-md-6">
+                                <h3 className="product-title">{selectedNotem.name}</h3>
+                                <p className="product-description">{selectedNotem.description}</p>
+                                <h4 className="price">current price: <span>{selectedNotem.price} TL</span></h4>
+                                <h5 className="sizes">sizes:
+                                    {sizes}
+                                </h5>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
