9a1c5342fe9cd7653b9cd501597c20cee700014d
hasip_timurtas <hasip.timurtas@gmail.com>
2018-12-12 01:01:55 +0300

    amni stok recently added item.name to item.stokKodu

--- src/components/amni-stok/RecentlyAdded.js
+++ src/components/amni-stok/RecentlyAdded.js
@@ -4,7 +4,7 @@ export default class RecentlyAdded extends React.Component {
     render() {
         var notes = this.props.notes.slice(0,15).map((note,i) => {
             return <a onClick={e=> this.props.showNote(note)} href="#" key={i} className="list-group-item recent-note" refs="notem">
-                        {note.name}
+                        {note.stokKodu}
                    </a>
         });
 
