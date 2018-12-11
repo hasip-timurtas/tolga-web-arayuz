5a3c8c4d0295c88de33689a8f05043a38f55deec
hasip_timurtas <hasip.timurtas@gmail.com>
2018-12-11 23:52:49 +0300

    server url local url

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -15,8 +15,8 @@ if (!firebase.apps.length) {
 }
 
 // server url
-let url = 'http://178.62.203.163:3005/'
-//url = 'http://localhost:3005/' // TEST
+//let url = 'http://178.62.203.163:3005/'
+const url = 'http://localhost:3005/' // TEST
 
 const db = firebase.database();
 const dbf = firebase.firestore();
