7dce9f41c226e005ee8427bf077e7b2224878cc0
hasip_timurtas <hasip.timurtas@gmail.com>
2018-12-12 01:25:48 +0300

    amni searchnote stokkodu

--- src/components/amni-stok/SingleNote.js
+++ src/components/amni-stok/SingleNote.js
@@ -4,7 +4,7 @@ export default class SingleNote extends React.Component {
     render() {
         return (
             <a onClick={e=> this.props.showNote(this.props.notem)} href="#" className="list-group-item recent-note" refs="notem">
-               <img width="50" src={this.props.notem.thumbImg} />       {this.props.notem.name}
+               <img width="50" src={this.props.notem.thumbImg} />       {this.props.notem.stokKodu}
             </a>
         )
     }
